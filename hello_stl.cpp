e9571a4f7766bf0c1dee4e5de597e99a03f2d451
mingo <1797375296@qq.com>
2022-08-30 14:19:40 +0800

    test test test!

--- hello_stl.cpp
+++ hello_stl.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> nums({1,2,3,4,5});
+    vector<int> nums({1,2,3,4,5, 6});
     for (int i = 0; i < nums.size(); i++)
         cout << nums[i] << " ";
     cout << endl;
